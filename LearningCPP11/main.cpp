9ad8930c75cb6d9956c3581e7cda7c68ea92f80c
Baosong Wu <bubbercn@163.com>
2016-07-08 13:33:18 +0800

    Project set up.

---
+++ LearningCPP11/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearningCPP11
+//
+//  Created by Baosong on 7/8/16.
+//  Copyright © 2016 Baosong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
